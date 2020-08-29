27254be5968faee358bc4f56e3b66f4a9b490cd7
Nancy <negativefnnancy@gmail.com>
2020-08-29 00:35:32 -0500

    finally some gravity

--- src/realtime_mode.c
+++ src/realtime_mode.c
@@ -13,6 +13,7 @@
 int entry_realtime (char *script_path) {
 
     /* nnvs stuff */
+    force_t *gravity;
     stage_t *stage;
     shape_t *shape_1;
     shape_t *shape_2;
@@ -34,6 +35,7 @@ int entry_realtime (char *script_path) {
     puts ("Ready for some realtime action!");
 
     /* setup the stage */
+    gravity    = create_gravity_force (make_vec2 (0, 1));
     shape_1    = create_rectangle (make_vec2 (1, 1));   /* square */
     shape_2    = create_rectangle (make_vec2 (0.5, 1)); /* tall rect */
     shape_3    = create_polygon   ();                   /* star */
@@ -51,15 +53,17 @@ int entry_realtime (char *script_path) {
     drawable_3 = create_drawable_group ();
     group_add_drawable (drawable_3, drawable_1);
     group_add_drawable (drawable_3, drawable_2);
-    cairo_matrix_scale (&(drawable_2->transformation), 0.8, 0.8);
+    cairo_matrix_scale (&(drawable_1->transformation), 0.5, 0.5);
+    cairo_matrix_scale (&(drawable_2->transformation), 0.3, 0.3);
 
     stage = create_stage (make_color (0.2, 0.2, 0.2, 1));
     add_drawable (stage, drawable_3);
     add_body     (stage, &body_1);
+    add_force    (stage, gravity);
 
     body_1.transformation = &(drawable_3->transformation);
     body_1.position     = make_vec2 (0, 0);
-    body_1.velocity     = make_vec2 (1, 0);
+    body_1.velocity     = make_vec2 (0, 0);
     body_1.acceleration = make_vec2 (0, 0);
     body_1.angle                = 0;
     body_1.angular_velocity     = 1;
@@ -128,6 +132,7 @@ quit:
     destroy_drawable (drawable_3);
     destroy_shape    (shape_1);
     destroy_shape    (shape_2);
+    destroy_force    (gravity);
 
     return EXIT_SUCCESS;
 }
