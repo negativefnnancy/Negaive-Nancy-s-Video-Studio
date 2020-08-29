1f39b1916dec40ff1d05324eee1ec76af88b415b
Nancy <negativefnnancy@gmail.com>
2020-08-28 23:50:40 -0500

    workin on phantom and interaction forces..

--- src/force.c
+++ src/force.c
@@ -24,3 +24,61 @@ force_t *create_force (void *data,
     force->destroy = destroy;
     return force;
 }
+
+void destroy_interaction_force (force_t *force) {
+
+    interaction_force_t *interaction_force = (interaction_force_t *) force->data;
+
+    /* polymorphic dispatch */
+    interaction_force->destroy (interaction_force);
+
+    free (interaction_force);
+}
+
+force_t *create_interaction_force (body_t *a,
+                                   body_t* b,
+                                   void *data,
+                                   force_apply_method_t *apply,
+                                   interaction_force_destructor_t *destroy) {
+
+    interaction_force_t *interaction_force =
+        (interaction_force_t *) calloc (1, sizeof (interaction_force_t));
+    interaction_force->a = a;
+    interaction_force->b = b;
+    interaction_force->data = data;
+    interaction_force->destroy = destroy;
+
+    return create_force (interaction_force, apply, destroy_interaction_force);
+}
+
+void apply_phantom_force (force_t *force) {
+
+    phantom_force_t *phantom_force = (phantom_force_t *) force->data;
+
+    /* TODO: for every target body, do:
+     *   phantom_force->apply (phantom_force, body);
+     */
+}
+
+void destroy_phantom_force (force_t *force) {
+
+    phantom_force_t *phantom_force = (phantom_force_t *) force->data;
+
+    /* polymorphic dispatch */
+    phantom_force->destroy (phantom_force);
+
+    free (phantom_force);
+}
+
+force_t *create_phantom_force (void *data,
+                               phantom_force_apply_method_t *apply,
+                               phantom_force_destructor_t *destroy) {
+
+    phantom_force_t *phantom_force =
+        (phantom_force_t *) calloc (1, sizeof (phantom_force_t));
+    phantom_force->data = data;
+    phantom_force->apply = apply;
+    phantom_force->destroy = destroy;
+
+    return create_force (phantom_force, apply_phantom_force, destroy_phantom_force);
+}
--- src/force.h
+++ src/force.h
@@ -1,6 +1,12 @@
 #ifndef FORCE_H
 #define FORCE_H
 
+#include "body.h"
+
+
+
+/* FORCE */
+
 struct force_t;
 
 /* polymorphic method types */
@@ -31,5 +37,74 @@ void destroy_force (force_t *force);
 force_t *create_force (void *data,
                        force_apply_method_t *apply,
                        force_destructor_t *destroy);
+
+
+
+/* INTERACTION FORCE */
+
+struct interaction_force_t;
+
+/* polymorphic method types */
+typedef void interaction_force_destructor_t (struct interaction_force_t *);
+
+/* an abstract interaction force that mediates energy exchange between two bodies */
+typedef struct interaction_force_t {
+
+    /* the bodies interacting */
+    body_t *a;
+    body_t *b;
+
+    /* polymorphic child struct */
+    void *data;
+
+    /* abstract destructor */
+    interaction_force_destructor_t *destroy;
+
+} interaction_force_t;
+
+/* free an interaction force instance */
+void destroy_interaction_force (force_t *force);
+
+/* instantiate an interaction force */
+force_t *create_interaction_force (body_t *a,
+                                   body_t* b,
+                                   void *data,
+                                   force_apply_method_t *apply,
+                                   interaction_force_destructor_t *destroy);
+
+
+
+/* PHANTOM FORCE */
+
+struct phantom_force_t;
+
+/* polymorphic method types */
+typedef void phantom_force_apply_method_t (struct phantom_force_t *, body_t *body);
+typedef void phantom_force_destructor_t   (struct phantom_force_t *);
+
+/* an abstract phantom force that injects energy into its target bodies */
+typedef struct phantom_force_t {
+
+    /* polymorphic child struct */
+    void *data;
+
+    /* abstract method to apply the phantom force to its targets */
+    phantom_force_apply_method_t *apply;
+
+    /* abstract destructor */
+    phantom_force_destructor_t *destroy;
+
+} phantom_force_t;
+
+/* apply the phantom force to its targets */
+void apply_phantom_force (force_t *force);
+
+/* free a phantom force instance */
+void destroy_phantom_force (force_t *force);
+
+/* instantiate a phantom force */
+force_t *create_phantom_force (void *data,
+                               phantom_force_apply_method_t *apply,
+                               phantom_force_destructor_t *destroy);
 
 #endif /* FORCE_H */
